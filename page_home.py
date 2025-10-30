c2c8205627b5bf80d1d547d39a1ad7983980a888
Lwyi2929 <lwyi2929@gmail.com>
2025-10-30 09:28:32 +0000

    modified:   page_home.py

--- page_home.py
+++ page_home.py
@@ -6,12 +6,12 @@ st.write("這是一個使用 Streamlit 建立的3D互動式地圖應用程式。
 
 
 # 直接將 MP4 影片的 URL 傳給 st.video()
-video_url = "https://i.imgur.com/1GoAB0C.mp4"
+video_url = "埋要.mp4"
 
 st.write(f"正在播放影片： {video_url}")
 
 st.video(video_url)
 
 # 直接將 照片的 URL 傳給 st.image()
-image_url = "https://i.imgur.com/uf1T4ND.png"
+image_url = "可愛小孩.jpg"
 st.image(image_url)
